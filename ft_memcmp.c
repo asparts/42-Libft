cc8aa30c2a3c6a63a69827fa68b4ece4d43d6701
markus <nummimarkus@gmail.com>
2023-07-05 00:10:31 +0300

    fixes to functions

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: mnummi <mnummi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 22:51:21 by mnummi            #+#    #+#             */
-/*   Updated: 2022/11/09 20:41:04 by mnummi           ###   ########.fr       */
+/*   Updated: 2023/07/05 00:07:55 by mnummi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t			i;
-	unsigned char	*str1;
-	unsigned char	*str2;
+	const unsigned char	*str1;
+	const unsigned char	*str2;
 
-	str1 = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
+	str1 = (const unsigned char *)s1;
+	str2 = (const unsigned char *)s2;
 	i = 0;
-	while (i <= n)
+	while (i < n)
 	{
 		if (str1[i] != str2[i])
 			return (str1[i] - str2[i]);

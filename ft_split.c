08e3cfc10b91be69be6709ccc6a1ac7eec21ab5a
markus <nummimarkus@gmail.com>
2023-07-06 19:08:49 +0300

    fixed ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,40 +6,78 @@
 /*   By: mnummi <mnummi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 21:38:23 by mnummi            #+#    #+#             */
-/*   Updated: 2023/07/06 18:52:59 by mnummi           ###   ########.fr       */
+/*   Updated: 2023/07/06 19:08:32 by mnummi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_count_words(char const *s, char c)
+{
+	int	i;
+	int	words;
+
+	i = 0;
+	words = 0;
+	while (s && s[i])
+	{
+		if (s[i] != c)
+		{
+			words++;
+			while (s[i] != c && s[i])
+				i++;
+		}
+		else
+			i++;
+	}
+	return (words);
+}
+
+static int	ft_size_word(char const *s, char c, int i)
+{
+	int	size;
+
+	size = 0;
+	while (s[i] != c && s[i])
+	{
+		size++;
+		i++;
+	}
+	return (size);
+}
+
+static void	ft_free(char **strs, int j)
+{
+	while (j-- > 0)
+		free(strs[j]);
+	free(strs);
+}
+
 char**	ft_split(char const *s, char c)
 {
-	const char *a[2];
-	char*	s_tmp;
-	char*	s_tmp2;
 	int	i;
+	int	words;
+	char	**strs;
+	int	size;
 	int	j;
 
-	s_tmp = malloc(ft_strlen(s) + 1);
-	s_tmp = malloc(ft_strlen(s) + 1);
-	a = malloc(ft_strlen(s) +1);
 	i = 0;
 	j = 0;
-	if (!s_tmp || !a)
+	words = ft_count_words(s, c);
+	if(!(strs = (char **)malloc((words + 1) * sizeof(char*))))
 		return (NULL);
-	while (s[i] != c)
+	while (++j < words)
 	{
-		s_tmp[i] = s[i];
-		i++;
+		while (s[i] == c)
+			i++;
+		size = ft_size_word(s, c, i);
+		if (!(strs[j] = ft_substr(s, i, size)))
+		{
+			ft_free(strs, j);
+			return (NULL);
+		}
+		i += size;
 	}
-	while (s[i])
-	{
-		s_tmp2[j] = s[i];
-		i++;
-		j++;
-	}
-	s_tmp2[j] = 0;
-	ft_strcpy(a[0], s_tmp);
-	ft_strcpy(a[i], s_tmp2);
-	return (a);
+	strs[j] = 0;
+	return (strs);
 }

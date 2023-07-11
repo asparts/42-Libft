dc3bec65b29c650b7ce4d0c80228820aa1d04ac9
markus <nummimarkus@gmail.com>
2023-07-11 18:50:06 +0300

    fixed ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mnummi <mnummi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 06:10:03 by mnummi            #+#    #+#             */
-/*   Updated: 2023/07/10 23:28:11 by mnummi           ###   ########.fr       */
+/*   Updated: 2023/07/11 18:48:53 by mnummi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,17 @@ void	ft_putnbr_fd(int n, int fd)
 	long	nbr;
 
 	nbr = n;
-	if (n < 0)
+	if (nbr == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else
 	{
-		ft_putchar_fd('-', fd);
-		nbr = nbr * -1;
+		if (n < 0)
+		{
+			ft_putchar_fd('-', fd);
+			nbr = nbr * -1;
+		}
+		if (nbr >= 9)
+			ft_putnbr_fd(nbr / 10, fd);
+		ft_putchar_fd(nbr % 10 + '0', fd);
 	}
-	if (nbr > 10)
-		ft_putnbr_fd(nbr / 10, fd);
-	ft_putchar_fd(nbr % 10 + 48, fd);
 }

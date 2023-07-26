fb7f2e920e58141ec592eebec5fce43b3e956d8b
markus <nummimarkus@gmail.com>
2023-07-26 10:50:04 +0300

    Minor fix to ft_putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mnummi <mnummi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 06:10:03 by mnummi            #+#    #+#             */
-/*   Updated: 2023/07/11 18:48:53 by mnummi           ###   ########.fr       */
+/*   Updated: 2023/07/26 10:48:48 by mnummi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	ft_putnbr_fd(int n, int fd)
 			ft_putchar_fd('-', fd);
 			nbr = nbr * -1;
 		}
-		if (nbr >= 9)
+		if (nbr >= 10)
 			ft_putnbr_fd(nbr / 10, fd);
 		ft_putchar_fd(nbr % 10 + '0', fd);
 	}

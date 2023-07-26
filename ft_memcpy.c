70bc8cf060b64a6fd070211dea3b5b1ed3ccddf9
markus <nummimarkus@gmail.com>
2023-07-26 11:14:38 +0300

    fix: didn't segfault when null parameter was sent

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mnummi <mnummi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 21:56:22 by mnummi            #+#    #+#             */
-/*   Updated: 2023/07/11 21:13:13 by mnummi           ###   ########.fr       */
+/*   Updated: 2023/07/26 11:14:28 by mnummi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (dest == NULL || src == NULL)
-		return (dest);
 	while (i < n)
 	{
 		*(char *)(dest + i) = *(char *)(src + i);
